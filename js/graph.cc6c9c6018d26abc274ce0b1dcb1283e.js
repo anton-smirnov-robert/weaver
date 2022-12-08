d64b4d402f5db0cc5d434001cba58b26c5b0cc68
anton-smirnov-robert <anton-smirnov-robert@users.noreply.github.com>
2022-12-08 12:16:06 +0000

    deploy: 0dbdbe3fd6fb99332acec94fc339cb597924c346

--- js/graph.cc6c9c6018d26abc274ce0b1dcb1283e.js
+++ js/graph.cc6c9c6018d26abc274ce0b1dcb1283e.js
@@ -62,9 +62,10 @@ async function drawGraph(baseUrl, isHome, pathColors, graphConfig) {
       if (d.id.startsWith(path)) {
         return colour
       }
-      if ( d.id == "/Where-am-I" ) { return "#513b16" }
-      if ( d.id == "/about-me" ) { return "#ffcf11" }
-      if ( d.id == "/manifesto" ) { return "#9b4f2e" }
+      if ( d.id == "/TODO" ) { return "#000000" }
+      if ( d.id == "/Where-am-I" ) { return "#ffcf11" }
+      if ( d.id == "/about-me" ) { return "#9b4f2e" } 
+      if ( d.id == "/manifesto" ) { return "#513b16" } 
     }
 
     return "var(--g-node)"
